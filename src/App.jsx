acd2f881180f58ba90b9f8208f22cc80a083963d
asuspro-16 <2413250743@qq.com>
2025-09-02 19:46:05 +0800

    chore: 清理项目文件并重构App组件
    删除冗余的文档、测试页面和旧设计文件
    移除LandingPage路由，简化App组件结构
    添加新的落地页index.html

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { useState } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import ChatPage from './pages/ChatPage/ChatPage'
-import LandingPage from './pages/LandingPage/LandingPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
+    <div className="App">
+      <ChatPage />
+    </div>
   )
 }
 
